8bc3e7d294befa3f06b3073ed4aae82a26056942
Maikoncarlos <maikonsaturno@gmail.com>
2023-03-08 22:02:02 -0300

    first commit

---
+++ src/main/java/com/github/maikoncarlos/springbootsqslocalstack/SpringBootSqsLocalstackApplication.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.springbootsqslocalstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqsLocalstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSqsLocalstackApplication.class, args);
+	}
+
+}
